a54ab522fef0da2779d553de1ebeb19de6c2d310
Robert Santos <robertdosantos09@gmail.com>
2023-02-15 18:18:48 -0300

    test: Adicionando novos testes unitário na camada service da coleção motocycle.

--- tests/unit/Services/MotorcycleService.test.ts
+++ tests/unit/Services/MotorcycleService.test.ts
@@ -5,12 +5,45 @@ import motorcycleMock from './mock/MotorcycleMock';
 import MotorcycleService from '../../../src/Services/MotorcycleService';
 
 describe('Testes do model Motorcycle', function () {
-  it('01 - Teste se é possivel criar um novo valor para a coleção', async function () {
+  it('01 - Teste se é retornado todos os valores da coleção', async function () {
+    sinon.stub(Model, 'find').resolves(motorcycleMock.motorcycleAll);
+    
+    const service = new MotorcycleService();
+    const result = await service.findAll();
+
+    expect(result).to.be.deep.equal(motorcycleMock.motorcycleAll);
+    sinon.restore();
+  });
+
+  it('02 - Teste se é retornado um valor da coleção', async function () {
+    sinon.stub(Model, 'find').resolves(motorcycleMock.motorcycleAll);
+
+    const service = new MotorcycleService();
+    const result = await service.findId(motorcycleMock.motorcycleAll[0].id);
+
+    expect(result.type).to.be.deep.equal(null);
+    expect(result.message).to.be.deep.equal(motorcycleMock.motorcycleAll[0]);
+    sinon.restore();
+  });
+
+  it('03 - Teste se é retornado um erro por não encontrar o id', async function () {
+    sinon.stub(Model, 'find').resolves([]);
+
+    const service = new MotorcycleService();
+    const result = await service.findId('634852326b35b59438fbea29');
+
+    expect(result.type).to.be.deep.equal(404);
+    expect(result.message).to.be.deep.equal('Motorcycle not found');
+    sinon.restore();
+  });
+
+  it('04 - Teste se é possivel criar um novo valor para a coleção', async function () {
     sinon.stub(Model, 'create').resolves(motorcycleMock.motorcycleAll[0]);
 
     const service = new MotorcycleService();
     const result = await service.create(motorcycleMock.motorcycleCreate);
 
     expect(result).to.be.deep.equal(motorcycleMock.motorcycleAll[0]);
+    sinon.restore();
   });
 });
--- tests/unit/Services/mock/MotorcycleMock.ts
+++ tests/unit/Services/mock/MotorcycleMock.ts
@@ -11,6 +11,25 @@ const motorcycleCreate = {
 const motorcycleAll = [{
   id: '6348513f34c397abcad040b2',
   ...motorcycleCreate,
+}, {
+  id: '634852326b35b59438fbea31',
+  model: 'Honda Cbr 1000rr',
+  year: 2011,
+  color: 'Orange',
+  status: true,
+  buyValue: 59.900,
+  category: 'Street',
+  engineCapacity: 1000,
 }];
 
-export default { motorcycleAll, motorcycleCreate };
+const upMotorcycle = {
+  model: 'Honda Cb 600f Hornet',
+  year: 2014,
+  color: 'Red',
+  status: true,
+  buyValue: 45.000,
+  category: 'Street',
+  engineCapacity: 600,
+};
+
+export default { motorcycleAll, motorcycleCreate, upMotorcycle };
